04791eee906be1fdf4bb8a5c75c0d6fb9f2294dc
Clémence Martin <martinclemence31@gmail.com>
2018-04-05 15:19:15 +0200

    Allow recompressed csv files

--- lzma-file.js
+++ lzma-file.js
@@ -5,7 +5,7 @@ class LzmaFile {
 
     static readSync(filename) {
         let buffer = fs.readFileSync(filename);
-        if (buffer.slice(0, 4).toString('hex') === '5d000004') {
+        if (buffer.slice(0, 3).toString('hex') === '5d0000') {
             buffer = Buffer.concat([buffer.slice(0, 9), Buffer.alloc(4), buffer.slice(9)]);
             buffer = lzma.decompress(buffer).toString('utf8');
         }
